bbbe8d0e06d249eb2fdd71ab947e793049a4b6f4
ElianMelo <elianmelomorais@gmail.com>
2020-05-15 19:17:40 -0300

    Leitura do volume

--- src/pontuacao/Programa.java
+++ src/pontuacao/Programa.java
@@ -9,9 +9,18 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		int compras;
+		double ticketMedio;
+		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println("");
+		
+		System.out.print("Quantas compras o cliente fez no último ano? ");
+		compras = sc.nextInt();
+		System.out.print("Qual o ticket médio? ");
+		ticketMedio = sc.nextDouble();
 		
 		sc.close();
 	}

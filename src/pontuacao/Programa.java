690b17ca1f67bf6650eeba909f0bcd0224466a5f
ElianMelo <elianmelomorais@gmail.com>
2020-05-15 19:32:30 -0300

    Score de volume de compras

--- src/pontuacao/Programa.java
+++ src/pontuacao/Programa.java
@@ -10,8 +10,10 @@ public class Programa {
 		Scanner sc = new Scanner(System.in);
 		
 		int compras, inadimplencia;
-		double ticketMedio;
+		int scoreCompra;
+		double ticketMedio, volumeCompras;
 		char formaPgto;
+		scoreCompra = 0;
 		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
@@ -28,6 +30,21 @@ public class Programa {
 		inadimplencia = sc.nextInt();
 		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
 		formaPgto = sc.next().charAt(0);
+		
+		volumeCompras = compras * ticketMedio;
+		
+		if (compras <= 0) {
+			scoreCompra = 0;
+		} else if (volumeCompras <= 3000 && compras < 2) {
+			scoreCompra = 20;
+		} else if (volumeCompras <= 3000 && compras >= 2) {
+			scoreCompra = 40;
+		} else if (volumeCompras > 3000) {
+			scoreCompra = 60;
+		}
+		
+		System.out.println("");
+		System.out.printf("Score de volume de compras = %d pontos", scoreCompra);
 		
 		sc.close();
 	}
